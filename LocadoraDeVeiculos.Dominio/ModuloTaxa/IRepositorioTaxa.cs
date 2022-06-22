d8c323e76b363b0208e11756dadd73f6ce1c8a57
Marcos Fernandes <marcosfernandes@uniplaclages.edu.br>
2022-06-22 16:14:46 -0300

    Implementada domínio de taxa para servir como base

---
+++ LocadoraDeVeiculos.Dominio/ModuloTaxa/IRepositorioTaxa.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Dominio.ModuloTaxa
+{
+    public class IRepositorioTaxa
+    {
+    }
+}
---
+++ LocadoraDeVeiculos.Dominio/ModuloTaxa/ValidadorTaxa.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Dominio.ModuloTaxa
+{
+    public class ValidadorTaxa : AbstractValidator<Taxa>
+    {
+        public ValidadorTaxa()
+        {
+            RuleFor(x => x.Descricao)
+                .NotNull().NotEmpty().MinimumLength(3);
+            RuleFor(x => x.Valor)
+                .NotNull().NotEmpty();
+
+        }
+    }
+}
