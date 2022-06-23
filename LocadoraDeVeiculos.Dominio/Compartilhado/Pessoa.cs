454e1586c19181cae6e8d2a6cb37299598a8ae43
JoseCavani <ze.carlos98@hotmail.com>
2022-06-23 14:26:59 -0300

    Classe PessoaBase

---
+++ LocadoraDeVeiculos.Dominio/Compartilhado/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Dominio.Compartilhado
+{
+    abstract class Pessoa<T> : EntidadeBase<T>
+    {
+       public string Nome { get; set; }
+       public string Endereco { get; set; }
+       public string Email { get; set; }
+       public string Telefone { get; set; }
+    }
+}
