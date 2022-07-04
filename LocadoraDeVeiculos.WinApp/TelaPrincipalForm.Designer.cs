55ec8e34a81e2183f7ec666e61ee68d24208d060
JoseCavani <ze.carlos98@hotmail.com>
2022-07-04 17:41:53 -0300

    Mudado a cor do rodape

--- LocadoraDeVeiculos.WinApp/TelaPrincipalForm.Designer.cs
+++ LocadoraDeVeiculos.WinApp/TelaPrincipalForm.Designer.cs
@@ -264,7 +264,7 @@
             // 
             // statusStripRodape
             // 
-            this.statusStripRodape.BackColor = System.Drawing.Color.Black;
+            this.statusStripRodape.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.statusStripRodape.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.statusStripRodape.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.labelRodape});
