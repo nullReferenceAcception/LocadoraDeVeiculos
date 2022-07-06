523ec24e5a93094c29408cfd510b1afb964acc8f
JoseCavani <ze.carlos98@hotmail.com>
2022-07-06 16:56:17 -0300

    muadado idea

--- LocadoraDeVeiculos.Dominio/Compartilhado/EntidadeBase.cs
+++ LocadoraDeVeiculos.Dominio/Compartilhado/EntidadeBase.cs
@@ -4,7 +4,7 @@ namespace LocadoraDeVeiculos.Dominio
 {
     public abstract class EntidadeBase<T>
     {
-        [JsonProperty(Order = -10)]
+        [JsonProperty(Order = -4)]
         public int Id { get; set; }
         public abstract override bool Equals(object? obj);
     }
--- LocadoraDeVeiculos.Dominio/Compartilhado/Pessoa.cs
+++ LocadoraDeVeiculos.Dominio/Compartilhado/Pessoa.cs
@@ -4,13 +4,13 @@ namespace LocadoraDeVeiculos.Dominio.Compartilhado
 {
     public abstract class Pessoa<T> : EntidadeBase<T>
     {
-       [JsonProperty(Order = -9)]
+       [JsonProperty(Order = -3)]
        public string? Nome { get; set; }
-        [JsonProperty(Order = -8)]
+        [JsonProperty(Order = -2)]
         public string? Endereco { get; set; }
-        [JsonProperty(Order = -7)]
+        [JsonProperty(Order = -1)]
         public string? Email { get; set; }
-        [JsonProperty(Order = -6)]
+        [JsonProperty(Order = 0)]
         public string? Telefone { get; set; }
 
         public Pessoa(string nome, string endereco, string email, string telefone)
